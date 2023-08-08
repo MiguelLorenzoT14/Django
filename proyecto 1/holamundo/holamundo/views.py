60b435c0e58be90d448516009c8243f87fd2de64
MiguelLorenzoT14 <lorenzo142114@hotmail.com>
2023-08-08 13:47:47 -0500

    agregandp urls saludo y despedida

--- proyecto 1/holamundo/holamundo/views.py
+++ proyecto 1/holamundo/holamundo/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def saludo(request):
-   return HttpResponse("hola Mundo"); 
+   return HttpResponse("hola Mundo")
+
+def despedida(request):
+   return HttpResponse("hasta luego")
