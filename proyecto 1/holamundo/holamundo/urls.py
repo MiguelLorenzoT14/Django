6354d955119b6fea91dd4245553a8c93fd304ba8
MiguelLorenzoT14 <lorenzo142114@hotmail.com>
2023-08-08 14:16:31 -0500

    Rutas con parametros

--- proyecto 1/holamundo/holamundo/urls.py
+++ proyecto 1/holamundo/holamundo/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', views.saludo, name='saludo'),
-    path('despedida/', views.despedida, name='despedida')
+    path('despedida/', views.despedida, name='despedida'),
+    path('adulto/<int:edad>/',views.adulto,name='adulto')
 ]
